8f283ea7076ca5403b8a8ae9dc54baa1222054fe
Sergei Bakhmatov <94bahmatov@gmail.com>
2023-05-23 18:18:21 +0500

    Add edit Task

--- RealmApp/Services/StorageManager.swift
+++ RealmApp/Services/StorageManager.swift
@@ -65,6 +65,21 @@ final class StorageManager {
         }
     }
     
+    func delete(_ task: Task) {
+        
+    }
+    
+    func edit(_ task: Task, newTaskTitle: String, newTaskNote: String) {
+        write {
+            task.title = newTaskTitle
+            task.note = newTaskNote
+        }
+    }
+    
+    func done(_ task: Task) {
+        
+    }
+    
     private func write(completion: () -> Void) {
         do {
             try realm.write {
